eaf73126b6249149f0a4d78ba8d68c69a37ae4f1
Eiman Abdelmoneim <eimanabdelmoneim@Eimans-MacBook-Air.local>
2015-03-17 21:58:15 -0500

    make it better

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.status(200).send('Slackbot is Alive!!!');
+});
+
+router.get('/hellobot', function(req,res,next){
+	var userName = req.body.user_name;
+	var botPayload = {
+			text: 'Hello, ' + userName + '!'
+	};
+	
+	if(userName !== 'slackbot') {
+		return res.status(200).json(botPayload);
+	} else {
+		return res.status(200).end();
+	}
+})
+
+module.exports = router;
