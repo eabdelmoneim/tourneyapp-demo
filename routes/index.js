b66a6a8e0606274d0322c18a45d15e9ad52c78a9
Eiman Abdelmoneim <eimanabdelmoneim@Eimans-MacBook-Air.local>
2015-03-24 20:11:33 -0500

    make it better

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.status(200).send('Slackbot is Alive!!!');
 });
 
+/* POST from slack - Hello */
 router.post('/hellobot', function(req,res,next){
 	var userName = req.body.user_name;
 	var botPayload = {
@@ -17,6 +18,40 @@ router.post('/hellobot', function(req,res,next){
 	} else {
 		return res.status(200).end();
 	}
+})
+
+/* POST from slack - follow */
+router.post('/follow', function(req, res, next) {
+	var userName = req.body.user_name;
+	var msgText = req.body.text;
+	
+	// find the team names using the @
+	var teamNames = msgText.match(/@\w+/g) ;
+	
+	var botPayload = null;
+	
+	// if no team name found send instructions
+	if(teamNames == null) {
+		botPayload = {
+			text: 'No team name for following was found in your message. To follow a team send \'follow @team\''	
+		};
+	}
+	
+	for(teamIndex=0; teamIndex<teamNames.length; teamIndex++) {
+		teamNames[teamIndex] = teamNames[teamIndex].substr(1);
+	}
+	
+	// if user doesn't exist create the new User
+	
+	// add the user as a follower to team
+	
+	// send response to user with the #channel for the team
+	botPayload = {
+		text: 'Congrats you can receive updates for your team at #' + teamNames[0]	
+	};
+	
+	return res.status(200).json(botPayload);
+	
 })
 
 module.exports = router;
