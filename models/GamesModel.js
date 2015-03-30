9168e9a7093368d570912567f4a97eb43804b119
Eiman Abdelmoneim <eimanabdelmoneim@Eimans-MacBook-Air.local>
2015-03-29 21:11:53 -0500

    moved model javascript files to new models/ directory

---
+++ models/GamesModel.js
@@ -0,0 +1,48 @@
+/**
+ * Data model service for games
+ */
+
+var games = [ {
+	id : 1,
+	homeId : 1,
+	visitorId : 2,
+	period : 'Not Started',
+	homescore : 0,
+	visitorscore : 0
+// start time?
+} ];
+
+var gamesService = {
+
+	getGame : function(id) {
+
+		for (var i = 0; i < games.length; i++) {
+			var nextGame = games[i];
+			if (games[i].id === id) {
+				return games[i];
+			}
+		}
+
+		return null;
+	},
+
+	setScoreForGame : function(id, homeScore, visitorScore) {
+		var game = getGame(id);
+
+		if (game != null) {
+			game.homescore = homeScore;
+			game.visitorscore = visitorScore;
+		}
+	},
+
+	setPeriodForGame : function(id, period) {
+		var game = getGame(id);
+
+		if (game != null) {
+			game.period = period;
+		}
+	}
+
+};
+
+module.exports = gamesService;
